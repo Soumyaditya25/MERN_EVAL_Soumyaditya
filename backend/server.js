de81fe7875d03bc24be58ae2b52d6a4ed7bb1fe8
Soumyaditya Batabyal <soumyaditya2021@gmail.com>
2025-05-05 13:34:40 +0530

    deployment update

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import { connectDB } from "./config/db.js";
 import productRoutes from "./routes/product.route.js";
 
@@ -9,6 +10,14 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); 
 app.use("/api/products", productRoutes);
+
+const dirname = path.resolve();
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 app.listen(PORT, () => {
 	connectDB();
